35a361cac69d849e7795ba735ffc96f93d956da0
Alexei <80886790+Alexei1R@users.noreply.github.com>
2023-10-09 17:58:45 +0300

    first commit

---
+++ src/atom_communication/atom_communication/tcp_servernode.py
@@ -0,0 +1,82 @@
+import rclpy
+from rclpy.node import Node
+import socket
+
+
+import socket
+import threading
+HOST = '0.0.0.0'
+PORT = 12345
+
+
+# Create a TCP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+print(f"Server is listening on {HOST}:{PORT}")
+clients = []
+IsConnected = False
+
+
+
+class TCPServerNode(Node):
+
+    def __init__(self):
+        super().__init__('tcp_server_node')
+        self.server_socket = None
+
+    def start_server(self, host, port):
+        while rclpy.ok():
+            # Add your code here
+            client_socket, client_address = server_socket.accept()
+            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,client_address,))
+            client_handler.start()
+            IsConnected = True
+
+            while IsConnected:
+                message = input("Server: ")
+                self.send_message(message)
+                if message == "exit":
+                    break
+
+
+    def handle_client(self, client_socket, client_address):
+        print(f"Accepted connection from {client_socket.getpeername() , client_address}")
+        clients.append(client_socket)
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            message = data.decode('utf-8')
+            print(f"Received from client: {message}")
+
+
+        print(f"Connection from {client_socket.getpeername()} closed.")
+        clients.remove(client_socket)
+        client_socket.close()
+        IsConnected = False
+
+        
+    def send_message(self,message):
+        for client in clients:
+            client.send(message.encode('utf-8'))
+
+    def cleanup(self):
+        if self.server_socket:
+            self.server_socket.close()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TCPServerNode()
+
+    try:
+        node.start_server('0.0.0.0', 12345)  # Change the host and port as needed
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.cleanup()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/atom_test/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'atom_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='toor',
+    maintainer_email='rusu.alexei.2002@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'atom_test = atom_test.atom_test:main'
+        ],
+    },
+)
