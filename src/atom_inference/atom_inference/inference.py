bdf8078bbec4ca0064c1880a0b417d09ea289b29
Alexei1R <alexei.rusu.2002@gmail.com>
2023-10-09 20:32:36 +0300

    fds

--- src/atom_inference/atom_inference/inference.py
+++ src/atom_inference/atom_inference/inference.py
@@ -21,7 +21,7 @@ host_outputs = []
 cuda_outputs = []
 bindings = []
 
-print("-------------------------------------", os.getcwd())
+os.chdir("/home/toor/atom_ws/src/atom_inference/atom_inference")
 
 
 class YoloTRT():
