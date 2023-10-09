355095fadf49e856ed06e0d369dc53844ea54823
Alexei1R <alexei.rusu.2002@gmail.com>
2023-10-09 19:25:03 +0300

    update

--- src/atom_inference/atom_inference/inference.py
+++ src/atom_inference/atom_inference/inference.py
@@ -1,84 +1,90 @@
 import sys
-import cv2
+import cv2 
 import imutils
+from yoloDet import YoloTRT
+from gstreamer import gstreamer_pipeline
+
+
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
-from yoloDet import YoloTRT
+
+from std_msgs.msg import Int16
 
 
-def gstreamer_pipeline(
-    sensor_id=0,
-    capture_width=1920,
-    capture_height=1080,
-    display_width=960,
-    display_height=540,
-    framerate=10,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc sensor-id=%d ! "
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            sensor_id,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-class YOLOInferenceNode(Node):
+
+
+# use path for library and engine file
+model = YoloTRT(library="yolov5/build/libmyplugins.so", engine="yolov5/build/best.engine", conf=0.5, yolo_ver="v5")
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+frame_count = 0
+
+
+
+class Inference(Node):
     def __init__(self):
-        super().__init__('yolo_inference_node')
+        super().__init__('inferece')
+        self.publisher_ = self.create_publisher(Int16, 'inference', 10)
         
-        self.model = YoloTRT(library="yolov5/build/libmyplugins.so", engine="yolov5/build/best.engine", conf=0.5, yolo_ver="v5")
-        self.cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-        self.publisher = self.create_publisher(Float32, 'inference_y_offset', 10)
-        self.timer = self.create_timer(1.0 / 10, self.inference_callback)  # Adjust the timer rate as needed
-        self.frame_count = 0
+        
 
-    def inference_callback(self):
-        ret, frame = self.cap.read()
-        self.frame_count += 1
+    def runIference(self):
+        while True:
+            ret, frame = cap.read()
+            frame_count += 1
 
-        if self.frame_count % 3 != 0:
-            return
+            if frame_count % 3 != 0:  # Skip every 2 out of 3 frames to achieve 10 fps processing
+                continue
+            frame = imutils.resize(frame, width=600)
+            detections, t = model.Inference(frame)
+            fps = 1 / t
+            
+            for obj in detections:
+                box = obj['box']
+                # Calculate the center of the bounding box
+                center_x = (box[0] + box[2]) / 2
+                center_y = (box[1] + box[3]) / 2
 
-        frame = imutils.resize(frame, width=600)
-        detections, t = self.model.Inference(frame)
+                # Calculate the offset from the center
+                offset_x = center_x - frame.shape[1] / 2
+                offset_y = center_y - frame.shape[0] / 2
 
-        for obj in detections:
-            box = obj['box']
-            center_x = (box[0] + box[2]) / 2
-            center_y = (box[1] + box[3]) / 2
-            offset_x = center_x - frame.shape[1] / 2
-            offset_y = center_y - frame.shape[0] / 2
+                offset_text = f"Offset: X={offset_x:.2f}, Y={offset_y:.2f}"
+                cv2.putText(frame, offset_text, (int(center_x), int(center_y)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
 
-            # Publish the Y offset
-            y_offset_msg = Float32()
-            y_offset_msg.data = offset_y
-            self.publisher.publish(y_offset_msg)
+            
+            
+            
+            
+            cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow("Output", frame)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+        
 
-        cv2.imshow("Output", frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            self.get_logger().info("Shutting down...")
-            self.cap.release()
-            cv2.destroyAllWindows()
-            self.timer.cancel()
+
+
+
+
+
+
+
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = YOLOInferenceNode()
-    rclpy.spin(node)
-    node.destroy_node()
+
+    inf_model = Inference()
+    inf_model.runIference()
+    # rclpy.spin(inf_model)
+    inf_model.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
+    cap.release()
+    cv2.destroyAllWindows() 
+
+
+
