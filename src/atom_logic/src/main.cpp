6c97b6fc4fc0ec26425c2bf2f14fc9ba366f4675
Alexei1R <rusu.alexei.2002@gmail.com>
2023-10-17 04:06:20 -0400

    test up

--- src/atom_logic/src/main.cpp
+++ src/atom_logic/src/main.cpp
@@ -7,6 +7,30 @@
 #include "atom_logic/FindLines.h"
 #include "atom_logic/WebCamStream.h"
 
+
+
+#define PID_KP 2.0f
+#define PID_KI 0.5f
+#define PID_KD 0.25f
+
+#define PID_TAU 0.02f
+
+#define PID_LIM_MIN -25.0f
+#define PID_LIM_MAX 25.0f
+
+#define PID_LIM_MIN_INT -5.0f
+#define PID_LIM_MAX_INT 5.0f
+
+#define SAMPLE_TIME_S 0.01f
+
+#define SIMULATION_TIME_MAX 4.0f
+
+int left = 200;
+int right = 400;
+
+
+
+
 class LineFollowerNode : public rclcpp::Node {
 public:
     LineFollowerNode() : Node("line_follower_node") {
