cc4a32973b44640824adc2bd1e6fd70c26b65b1d
Alexei1R <rusu.alexei.2002@gmail.com>
2023-10-19 05:39:35 -0400

    qwerty

--- src/atom_communication/launch/communication.launch.py
+++ src/atom_communication/launch/communication.launch.py
@@ -1,7 +1,5 @@
-import launch
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -9,16 +7,9 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='atom_communication',
-            executable='tcp_servernode',
-            name='tcp_servernode',
+            node_executable='tcp_servernode',
+            node_name='tcp_servernode',
             output='screen',
-            condition=IfCondition(LaunchConfiguration('use_tcp_servernode')),
         ),
-        # Node(
-        #     package='atom_communication',
-        #     executable='serial_node',
-        #     name='serial_node',
-        #     output='screen',
-        #     condition=IfCondition(LaunchConfiguration('use_serial_node')),
-        # ),
+        
     ])
--- src/atom_logic/launch/atom_core_logic.launch.py
+++ src/atom_logic/launch/atom_core_logic.launch.py
@@ -3,6 +3,13 @@ from launch.actions import DeclareLaunchArgument, LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
@@ -10,6 +17,10 @@ def generate_launch_description():
             node_executable='atom_core_logic_node',
             node_name='atom_core_logic_node',
             output='screen',
+        ),
+        # communication.launch.py
+         IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([get_package_share_directory('atom_communication'), '/launch/communication.launch.py']),
         ),
         
     ])
