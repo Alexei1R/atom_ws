cea2528b3f1187a435c7813658d76bfb774408f3
Alexei1R <rusu.alexei.2002@gmail.com>
2023-10-19 12:08:20 -0400

    corect

--- src/atom_communication/atom_communication/serial_node.py
+++ src/atom_communication/atom_communication/serial_node.py
@@ -18,7 +18,7 @@ class SerialPublisher(Node):
             # Send the received message to the serial port
             # print msg.data
             print(f"Sending to serial port: {msg.data}")
-            self.serial_port.write(b'1:' + msg.data.encode('ascii'))
+            self.serial_port.write(msg.data.encode('ascii'))
 
         except Exception as e:
             self.get_logger().error(f"Error writing to serial port: {str(e)}")
