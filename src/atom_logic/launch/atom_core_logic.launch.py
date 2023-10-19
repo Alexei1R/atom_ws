a8a755af29012dc56ca7063de6eff2f78b272a5d
Alexei1R <rusu.alexei.2002@gmail.com>
2023-10-19 05:44:12 -0400

    test

--- src/atom_logic/launch/atom_core_logic.launch.py
+++ src/atom_logic/launch/atom_core_logic.launch.py
@@ -18,9 +18,15 @@ def generate_launch_description():
             node_name='atom_core_logic_node',
             output='screen',
         ),
-        # communication.launch.py
-         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([get_package_share_directory('atom_communication'), '/launch/communication.launch.py']),
+        Node(
+            package='atom_communication',
+            node_executable='tcp_servernode',
+            node_name='tcp_servernode',
+            output='screen',
         ),
+        # # communication.launch.py
+        #  IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([get_package_share_directory('atom_communication'), '/launch/communication.launch.py']),
+        # ),
         
     ])
