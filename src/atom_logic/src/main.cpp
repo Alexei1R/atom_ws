4b94c68431a1c838bbe1bc59c1f848d3752f2f7a
Alexei1R <rusu.alexei.2002@gmail.com>
2023-10-19 11:51:54 -0400

    asdf

--- src/atom_logic/src/main.cpp
+++ src/atom_logic/src/main.cpp
@@ -70,7 +70,7 @@ public:
 				sprintf(serialBuffer, "1:%d\n" , 90 - (uint8_t)abs(std::round(pid.out)));
 			}
 
-            // std::cout<<serialBuffer<<std::endl;
+            std::cout<<serialBuffer<<std::endl;
             //publish pid
             auto pid_msg = std_msgs::msg::String();
             pid_msg.data = serialBuffer;
